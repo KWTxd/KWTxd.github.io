ad4b158579c48784523c52fd5cae823f6391241c
Kaashwastaken <119799595+KWTxd@users.noreply.github.com>
2022-12-05 17:53:44 +0300

    Update and rename ui.lua to deltaui.lua

--- Aureus/deltaui.lua
+++ Aureus/deltaui.lua
@@ -1,5 +1,10 @@
-<html><head><meta name="color-scheme" content="light dark"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">-- // Load
-
+<html>
+<head>
+<meta name="color-scheme" content="light dark">
+</head>
+<body>
+<pre style="word-wrap: break-word; white-space: pre-wrap;">
+-- // Load
 local startupArgs = ({...})[1] or {}
 
 if getgenv().library ~= nil then
